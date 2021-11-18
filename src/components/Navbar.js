3f99fa466ad86b3204dbd47101fe2f55fe292143
Vinicius Ferreira <viniciuferreira32@gmail.com>
2021-11-17 22:17:55 -0300

    Projeto commit no projeto

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react'
+
+function Navbar() {
+    return (
+        <>
+            <nav className="navbar">
+                        
+            </nav>
+            nav.navbar  
+        </>
+    )
+}
+
+export default Navbar
